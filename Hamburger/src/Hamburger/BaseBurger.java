75e25d61b2ee6b0e0d856c14e34f254e8a770895
David Aragundy <waldaara@espol.edu.ec>
2023-10-27 23:18:27 -0500

    integrante 1 y 3 cambios

--- Hamburger/src/Hamburger/BaseBurger.java
+++ Hamburger/src/Hamburger/BaseBurger.java
@@ -9,12 +9,14 @@ public class BaseBurger {
     private String meat;
     private double base_price;
     private double total_price;
+    private String size;
 
-    BaseBurger(String name, String breadRollType,String meat,double basePrice){
+    BaseBurger(String name, String breadRollType,String meat,double basePrice, String size){
         this.name = name;
         this.breadRollType =  breadRollType;
         this.meat = meat;
         this.base_price = basePrice;
+        this.size = "Normal";
     }
     public List<Integer> additions = new ArrayList<>();
     private final double addedPrice = 23;
@@ -52,4 +54,7 @@ public class BaseBurger {
         return base_price;
     }
 
+    public void setSize(String size) {
+        this.size = size;
+    }
 }
